707a91a96f7ffd99e02af687cd65bffad3ee85f4
Christopher Britt <christopher.brittjr@outlook.com>
2018-10-24 10:49:32 -0400

    Lesson 9 Using class constructors with derived classes

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -10,12 +10,25 @@ namespace Engine
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public Location(int id, string name, string description)
+        public string Description { get; set; }
+
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationNorth { get; set; }
+        public Location LocationEast { get; set; }
+        public Location LocationSouth { get; set; }
+        public Location LocationWest { get; set; }
+
+        public Location(int id, string name, string description, 
+                        Item itemRequiredToEnter = null, Quest questAvailableHere = null, Monster monsterLivingHere = null)
         {
             ID = id;
             Name = name;
             Description = description;
+            ItemRequiredToEnter = itemRequiredToEnter;
+            QuestAvailableHere = questAvailableHere;
+            MonsterLivingHere = monsterLivingHere;
         }
     }
 }
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -21,13 +21,9 @@ namespace SuperAdventure
             InitializeComponent();
 
             Location location = new Location(1, "Home","This is your House");
+
+            _player = new Player(10, 10, 20, 0, 1);
             
-            _player = new Player();
-            _player.CurrentHitPoints = 10;
-            _player.MaximumHitPoints = 10;
-            _player.Gold = 20;
-            _player.ExperiencePoints = 0;
-            _player.Level = 1;
 
             lblHitPoints.Text = _player.CurrentHitPoints.ToString();
             lblGold.Text = _player.Gold.ToString();
